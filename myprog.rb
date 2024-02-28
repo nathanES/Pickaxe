5a00166815b75423a73441755da19a105a3b5997
nathan <nathan.esslinger67@gmail.com>
2024-02-28 19:34:54 +0100

    array hashes

--- myprog.rb
+++ myprog.rb
@@ -8,3 +8,19 @@ end
 
 puts say_hello_goodbye("John")
 puts say_hello_goodbye("Paul")
+
+#Array
+a=[1, "cat", 3.14] # can have multiple type in it
+a[2] = nil
+puts "The array #{a.inspect}"
+
+#Hashes
+instrument_section = {
+  "cello" => "string",
+  "clarinet" => "woodwind",
+  "drum" => "percussion",
+  "oboe" => "woodwind",
+  "trumpet" => "brass",
+  "violin" => "string"
+}
+puts instrument_section["clarinet"]
