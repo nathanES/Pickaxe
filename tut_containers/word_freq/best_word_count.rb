25d9cd4e5cb08b2ad7ceab79e8a85f3bc5b40242
nathan <nathan.esslinger67@gmail.com>
2024-03-09 11:20:14 +0100

    Block and enumeration beginning

---
+++ tut_containers/word_freq/best_word_count.rb
@@ -0,0 +1,14 @@
+require_relative "words_from_string"
+raw_text = "The problem breaks down into two parts. First, given some text as a string, return a list of words. That sounds like an array. Then, build a count for each distinct word. That sounds like a use for a hash---we can index it with the word and use the corresponding entry to keep a count."
+
+word_list = words_from_string(raw_text)
+counts = word_list.tally
+sorted = counts.sort_by do |word, count|
+    count
+end
+top_five = sorted.last(5)
+puts top_five.reverse.map { |word, count| "#{word}: #{count}"}
+#puts top_five.reverse.map do |word, count| 
+#    "#{word}: #{count}"
+#end
+# et il y a une différence entre {} et do end
